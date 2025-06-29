aa4ffc9ec6ffacdfbd5e11f1053f18606a6da736
aaman <aamanmujawar38@gmail.com>
2025-06-29 11:19:47 +0530

    api response mongo conneciton

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import dotenv from "dotenv";
 import express from "express"
 import connectDB from "./db/conn.js";
+// import app from "./app.js";
 
 
 
 connectDB()
-/*
-const app = express();
-;(async () => {
-    try{
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       app.on("error", () =>{
-        console.log("ERR",error);
-        throw error
-       })
-
-       app.listen(process.env.PORT,()=>{
-        console.log("App is listent the port");
-       })
-    }catch(err){
-        console.log(err);
-        throw err;
-    }
-})()
-    */
+.then(() =>{
+   app.listen(process.env.PORT || 8000,()=>{
+    console.log("server started")
+   })
+})
+.catch((err) =>{
+    console.log(err);
+    
+})
